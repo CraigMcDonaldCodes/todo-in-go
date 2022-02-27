1dcd75849e1ad9deafe482b360a57c8ba1b3aeb6
Craig McDonald <craig@mcdonald.codes>
2022-02-27 01:09:37 +0000

    Main package updated to register handlers

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/craigmcdonaldcodes/todo-in-go/handlers"
+	"github.com/craigmcdonaldcodes/todo-in-go/utils"
 )
 
 func main() {
-	fmt.Println("Todo in Go")
+
+	utils.DisplayBanner()
+
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/add", handlers.Add)
+	http.HandleFunc("/delete", handlers.Delete)
+	http.HandleFunc("/purge", handlers.Purge)
+
+	http.ListenAndServe("localhost:8080", nil)
 }
